eb0a9e203e0fccda5a36dd9c49225b08b5d5f828
angkor <angkor@DESKTOP-5PF98GM>
2022-07-13 14:53:17 +0800

    TcpClient

--- C#/Echo/Echo/Form1.cs
+++ C#/Echo/Echo/Form1.cs
@@ -1,16 +1,33 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace Echo
 {
     public partial class Form1 : Form
     {
-        ReceiveMsg receiveMsg= new ReceiveMsg();
+        private TcpClient TcpClient { get; }
+        ReceiveMsg receiveMsg = new ReceiveMsg();
 
         public Form1()
         {
             InitializeComponent();
+            TcpClient = new TcpClient();
         }
 
         private async void btnOK_Click(object sender, EventArgs e)
         {
+            byte[]? continueBytes = new byte[] { (byte)'H', (byte)'T', (byte)'T', (byte)'P', (byte)'/', (byte)'1', (byte)'.', (byte)'1', (byte)' ', (byte)'1', (byte)'0', (byte)'0', (byte)' ', (byte)'C', (byte)'o', (byte)'n', (byte)'t', (byte)'i', (byte)'n', (byte)'u', (byte)'e', (byte)'\r', (byte)'\n', (byte)'\r', (byte)'\n' };
+
+            IPEndPoint? endPoint = new IPEndPoint(IPAddress.Parse("106.13.130.51"), 8888);//°Ù¶È
+            try
+            {
+                TcpClient.Connect(endPoint);
+                TcpClient.Client.Send(continueBytes);
+            }
+            catch (Exception e)
+            {
+
+            }
             int t = await receiveMsg.WaitForDataAsync();
         }
     }
