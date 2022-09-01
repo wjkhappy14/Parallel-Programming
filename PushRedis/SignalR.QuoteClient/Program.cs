27c349abeddf2d58e3a1772593fcc1744137e1f6
angkor <angkor@DESKTOP-5PF98GM>
2022-09-01 16:41:18 +0800

    SignalR.QuoteClient

---
+++ PushRedis/SignalR.QuoteClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace SignalR.QuoteClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                args = new string[1];
+                args[0] = "dynamic";//streaming chat upload hubT talk  dynamic
+            }
+
+            if (args.Contains("--debug"))
+            {
+                Console.WriteLine($"Ready for debugger to attach. Process ID: {Process.GetCurrentProcess().Id}");
+                Console.Write("Press ENTER to Continue");
+                Console.ReadLine();
+                args = args.Except(new[] { "--debug" }).ToArray();
+            }
+
+            var app = new CommandLineApplication();
+            app.FullName = "SignalR Client Samples";
+            app.Description = "Client  for SignalR";
+
+            MessagesConnectionHandler.Register(app);//chat
+            HubSample.Register(app);//hubT
+            TalkSample.Register(app);//talk
+            DynamicSample.Register(app);//dynamic
+            StreamingSample.Register(app);//streaming
+            UploadSample.Register(app);//upload
+
+            app.Command("help", cmd =>
+            {
+                cmd.Description = "Get help for the application, or a specific command";
+
+                var commandArgument = cmd.Argument("<COMMAND>", "The command to get help for");
+                cmd.OnExecute(() =>
+                {
+                    app.ShowHelp(commandArgument.Value);
+                    return 0;
+                });
+            });
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+    }
+}
---
+++ PushRedis/SignalR.QuoteClient/StreamingSample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace SignalR.QuoteClient
+{
+
+
+    /// <summary>
+    /// http://localhost:5000/streaming
+    /// </summary>
+    internal class StreamingSample
+    {
+        internal static void Register(CommandLineApplication app)
+        {
+            app.Command("streaming", cmd =>
+            {
+                cmd.Description = "Tests a streaming connection to a hub";
+
+                var baseUrlArgument = cmd.Argument("<BASEURL>", "The URL to the Chat Hub to test");
+
+                cmd.OnExecute(() => ExecuteAsync(baseUrlArgument.Value));
+            });
+        }
+
+        public static async Task<int> ExecuteAsync(string baseUrl)
+        {
+            baseUrl = string.IsNullOrEmpty(baseUrl) ? "http://localhost:5000/streaming" : baseUrl;
+            var connection = new HubConnectionBuilder()
+                .WithUrl(baseUrl)
+                .Build();
+
+            await connection.StartAsync();
+
+            var reader = await connection.StreamAsChannelAsync<int>("ChannelCounter", 10, 2000);
+
+            while (await reader.WaitToReadAsync())
+            {
+                while (reader.TryRead(out var item))
+                {
+                    Console.WriteLine($"received: {item}");
+                }
+            }
+
+            return 0;
+        }
+    }
+}
